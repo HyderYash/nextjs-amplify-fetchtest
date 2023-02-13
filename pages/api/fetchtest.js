e48e30f4d3f9382389a2926fd16994d13cf65b8a
HyderYash <yashsharma.karate@gmail.com>
2023-02-13 16:31:49 +0530

    DONE

--- pages/api/fetchtest.js
+++ pages/api/fetchtest.js
@@ -1,25 +1,25 @@
-import fetch from "cross-fetch";
+// import fetch from "cross-fetch";
 import commercelayerjsAuth from "@commercelayer/js-auth";
-import Cors from "cors";
+// import Cors from "cors";
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
+// // Initializing the cors middleware
+// const cors = Cors({
+//   methods: ["GET", "HEAD"],
+// });
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
 
-      return resolve(result);
-    });
-  });
-}
+//       return resolve(result);
+//     });
+//   });
+// }
 
 const getCommerceLayerAccessToken = async () => {
   try {
@@ -42,7 +42,7 @@ const getCommerceLayerAccessToken = async () => {
 
 export default async function handler(req, res) {
   // Run the middleware
-  await runMiddleware(req, res, cors);
+  // await runMiddleware(req, res, cors);
   const accessToken = await getCommerceLayerAccessToken();
   const API_URL = `${process.env.CL_BASE_ENDPOINT}/api/skus/${process.env.CL_SKU_ID}?include=delivery_lead_times`;
   await fetch(API_URL, {

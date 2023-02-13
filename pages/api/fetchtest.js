7e26daba63b3fd9d5489a44ef12a86262169d886
HyderYash <yashsharma.karate@gmail.com>
2023-02-13 15:58:39 +0530

    Added Cors

--- pages/api/fetchtest.js
+++ pages/api/fetchtest.js
@@ -1,5 +1,25 @@
 import fetch from "cross-fetch";
 import commercelayerjsAuth from "@commercelayer/js-auth";
+import Cors from "cors";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 const getCommerceLayerAccessToken = async () => {
   try {
@@ -17,30 +37,27 @@ const getCommerceLayerAccessToken = async () => {
 };
 
 export default async function handler(req, res) {
-  try {
-    const accessToken = await getCommerceLayerAccessToken();
-    const API_URL = `${process.env.CL_BASE_ENDPOINT}/api/skus/${process.env.CL_SKU_ID}?include=delivery_lead_times`;
-    fetch(API_URL, {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.api+json",
-        Authorization: `Bearer ${accessToken}`,
-      },
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+  const accessToken = await getCommerceLayerAccessToken();
+  const API_URL = `${process.env.CL_BASE_ENDPOINT}/api/skus/${process.env.CL_SKU_ID}?include=delivery_lead_times`;
+  await fetch(API_URL, {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.api+json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((res) => {
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return res.json();
     })
-      .then((res) => {
-        if (res.status >= 400) {
-          throw new Error("Bad response from server");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        res.status(200).json({ status: 200, data });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  } catch (err) {
-    res.status(500);
-    res.send(err);
-  }
+    .then((data) => {
+      res.status(200).json({ status: 200, data });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
